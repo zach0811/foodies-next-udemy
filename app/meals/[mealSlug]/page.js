499105471902f1bb3b26fbc588963df9416cb9e2
Zach Wilkins <zach.d.wilkins@gmail.com>
2024-04-04 08:09:55 -0400

    first commit for foodies app

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,9 @@
+const MealDetailsPage = () => {
+    return (
+        <div>
+        <h1>Meal Details Page</h1>
+        </div>
+    );
+    }
+
+export default MealDetailsPage;
---
+++ app/meals/page.js
@@ -0,0 +1,9 @@
+const ShareMealsPage = () => {
+  return (
+    <div>
+      <h1>Share Meals Page</h1>
+    </div>
+  );
+}
+
+export default ShareMealsPage;
